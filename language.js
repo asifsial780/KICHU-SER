62a2119d400bab97242fcefaa7076b54c43f57d2
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2022-02-22 20:59:25 +0530

    🛠️

--- language.js
+++ language.js
@@ -2,18 +2,22 @@ const Config = require('./config');
 const fs = require('fs');
 const chalk = require('chalk');
 
-if (fs.existsSync('./language/' + Config.LANG + '.json')) {
-    console.log(
-        chalk.green.bold('Loading ' + Config.LANG + ' language...')
-    );
+try {
+  if (fs.existsSync('./language/' + Config.LANG + '.json')) {
+      console.log(
+          chalk.green.bold('Loading ' + Config.LANG + ' language...')
+      );
 
-    var json = JSON.parse(fs.readFileSync('./language/' + Config.LANG + '.json'));
-} else {
-    console.log(
-        chalk.red.bold('You entered an invalid language. English language was chosen.')
-    );
+      var json = JSON.parse(fs.readFileSync('./language/' + Config.LANG + '.json'));
+  } else {
+      console.log(
+          chalk.red.bold('You entered an invalid language. English language was chosen.')
+      );
 
-    var json = JSON.parse(fs.readFileSync('./language/EN.json'));
+      var json = JSON.parse(fs.readFileSync('./language/EN.json'));
+  }
+} catch (e) {
+   console.error(e);
 }
 
 function getString(file) {
