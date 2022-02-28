f3cdc055c337fdc1fb91acc42959577c443c58bf
KichuSer <97310543+Itsme-soman@users.noreply.github.com>
2022-02-28 17:26:30 +0530

    try.....!

--- plugins/song.js
+++ plugins/song.js
@@ -8,7 +8,7 @@ Asena.addCommand({pattern: 'song', fromMe: false,dontAddCommandList: true}, (asy
     var r_text = new Array ();    
 r_text[0] = "\nEda eda, mandankonappi.. Nee enne kooduthal mandan aakkatheda\n";
 r_text[1] = "kichune tholpikkaan ningalkavilla. Jeevitathil pinkiye tholpichittundu… palarum… palavattom…😥😥 aarellum aaa sed bgm onn iduo...";
-r_text[2] = "Kichu says onnula poda😘";
+r_text[2] = "Kichu says onnula poda😌";
 r_text[3] = "Kichune oralku koduthekkuvann!";
 r_text[4] = "hi, how are you";
 r_text[5] = "njan pani eduth maduth ennikk korch rest theroo...🥵🥵";
@@ -17,9 +17,9 @@ r_text[7] = "താനാരാണെന്ന് തനിക്ക് അറ�
 r_text[8] = "\n enne vilicho...?🧐 \n";
 r_text[9] = "ente per Kichu,ninte per donkey, backi ellarum monkey😼😼";
 r_text[10] = "nee vilicha enikk villi kelkathe irrikkaan pattuo";
-r_text[11] = "do you know who is my creator..? its none other than saidali";
+r_text[11] = "do you know who is my creator..? its kichuser 😌";
 r_text[12] = "രണ്ട് ബക്കറ്റ് നിറയെ വെള്ളമുണ്ട്. അതിൽ ഒരു ബക്കറ്റിനു ദ്വാരമുള്ളതാണ്. എന്നാൽ ദ്വാരമുള്ള ബക്കറ്റിൽ നിന്നും വെള്ളം പോകുന്നില്ല. കാരണം എന്താണ്? para ninak vivaram indo nokkatte";
-r_text[13] = ".song adich thangalude fav song name para kichu ath download aakki theraam";
+r_text[13] = ".song adich thangalude fav song name para kichu ser ath download aakki tharum";
 r_text[14] = "ഹിന്ദിക്കാർ പോക്കറ്റിലും മലയാളികൾ അടുപ്പിലും വെക്കുന്ന സാധനം എന്ത്..?  ennallum ath enthaayikkum...🤔🤔";
 r_text[15] = "hi.. ningal ente fan aano..?";
 r_text[16] = "ivide aaro ente per villichallo.. nee aano..?";
