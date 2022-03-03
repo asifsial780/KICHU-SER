cf8aac4f5207d1bbe43e15eba1ca646d0052fd49
KichuSer <97310543+Itsme-soman@users.noreply.github.com>
2022-03-03 23:47:28 +0530

    Try....!!

---
+++ plugins/number.js
@@ -0,0 +1,20 @@
+//modified @ chunkindepadayali (liza mwol™)©
+//re coded by @ itsmesoman ©kichuser
+
+const Lizamwol = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+Lizamwol.addCommand({ pattern: 'number ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+
+//coded by saidali
+const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN: KICHUSER [BOT OWNER]\n' // full name
+            + 'ORG: ᴋɪᴄʜᴜꜱᴇʀ™;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid= '+Config.NU+' :+1 (762) 339-3587\n' // 
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid,{displayname: "KICHUSER [BOT OWNER]", vcard: vcard}, MessageType.contact , {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "ᴋɪᴄʜᴜꜱᴇʀ™", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('https://i.imgur.com/RtfHvLR.jpeg')}}}});
+}))
