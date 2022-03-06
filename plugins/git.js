aad729cbe913feb053ec799422458fdad4903607
KichuSer <97310543+Itsme-soman@users.noreply.github.com>
2022-03-06 21:38:39 +0530

    No updates...!

--- plugins/git.js
+++ plugins/git.js
@@ -11,17 +11,19 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Github* *(setup)* : *https://github.com/Itsme-soman/KICHU-SER*
 
-*Whatsapp group : https://chat.whatsapp.com/GgHXTY2Wo4rBhZGn8W2r9b*
-
-*Owner number : wa.me/919961857267*
+*Whatsapp group : https://chat.whatsapp.com/IGF65nLxwPeITjnZlUbnr4*
 
 *Owner number : wa.me/918075379950*
 
 *Owner number : wa.me/918714183251*
 
-*Audio comnds : https://github.com/Itsme-soman/medias/tree/master/uploads*
+*Owner number : wa.me/2349053311892*
 
-*Sticker cmnds : https://github.com/Itsme-soman/medias/tree/master/stickers*
+*Supporter number : wa.me/919633927168*
+
+*Audio commands : https://github.com/Itsme-soman/medias/tree/master/uploads*
+
+*Sticker commands : https://github.com/Itsme-soman/medias/tree/master/stickers*
 `}) 
 
 }));
