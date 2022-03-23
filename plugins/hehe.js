7782106cab226273ce5c76722a65820b868c3b2e
KichuSer <97310543+Itsme-soman@users.noreply.github.com>
2022-03-23 16:59:59 +0530

    Eh!new!

---
+++ plugins/hehe.js
@@ -0,0 +1,62 @@
+/* Copyright (C) 2022 Kichu (U can take but give credits!).
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Kichu
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+   
+Asena.addCommand({pattern: 'hack', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage('@{} *Beniwal, Gathered Information For Your Chosen Account!*');
+    await message.sendMessage('```Kichu Ser Bot```');
+    await message.sendMessage('*DATA*');
+    await message.sendMessage('*HACKING*');
+    await message.sendMessage('*The Target System Hacking Now!*');
+    await message.sendMessage('```𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 3%```');
+    await message.sendMessage('```𝔾𝕒𝕝𝕝𝕖𝕣𝕪: 7%```');
+    await message.sendMessage('```𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕤: 5%```');
+    await message.sendMessage('```𝔽𝕚𝕝𝕖𝕤: 4%```');
+    await message.sendMessage('```ℂ𝕒𝕞𝕖𝕣𝕒: 5%```');
+    await message.sendMessage('*Process Running...*');
+    await message.sendMessage('```𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 16%```');
+    await message.sendMessage('```𝔾𝕒𝕝𝕝𝕖𝕣𝕪: 27%```');
+    await message.sendMessage('```𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕤: 24%```');
+    await message.sendMessage('```𝔽𝕚𝕝𝕖𝕤: 38%```');
+    await message.sendMessage('```ℂ𝕒𝕞𝕖𝕣𝕒: 31%```');
+    await message.sendMessage('*Process Running...*');
+    await message.sendMessage('```𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: 32%```');
+    await message.sendMessage('```𝔾𝕒𝕝𝕝𝕖𝕣𝕪: 21%```');
+    await message.sendMessage('```𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕤: 34%```');
+    await message.sendMessage('```𝔽𝕚𝕝𝕖𝕤: 38%```');
+    await message.sendMessage('```ℂ𝕒𝕞𝕖𝕣𝕒: 38%```');
+    await message.sendMessage('*Deleting System’s Security System*');
+    await message.sendMessage('```Phone 80%```');
+
+    await new Promise(r => setTimeout(r, 1000));
+
+    await message.sendMessage('*Wait few minutes!*');
+    await message.sendMessage('*Connection Established!*');
+    await message.sendMessage('```𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: %100```');
+    await message.sendMessage('```𝔾𝕒𝕝𝕝𝕖𝕣𝕪: %100```');
+    await message.sendMessage('```𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕤: %100```');
+    await message.sendMessage('```𝔽𝕚𝕝𝕖𝕤: %100```');
+    await message.sendMessage('```ℂ𝕒𝕞𝕖𝕣𝕒: %100```');
+    await message.sendMessage('@⁨{} *Saved Targeted Account Information!*');
+    await message.sendMessage('*Sending LOG Documents*');
+    await message.sendMessage('```Hehe Nthada Mwonu Fear Ayo Ni Eh!!!```');
+    await message.sendMessage('```Pedikanda Mwona It's Prenk Ellam Kichunte kalikal..!!😹⚡```');
+
+    await new Promise(r => setTimeout(r, 1000));
+
+    await message.sendMessage('*LOG Documents Created! You Can Access It In Your Database.*');
+    await message.sendMessage('```Pinnem pedicho venda kutta oky bye!```');
+
+}));
+
+Asena.addCommand({pattern: 'infohack', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage("*Codded by Kichu*\n*Prank your friends with hack😹⚡* ```Credits:Kichu Ser```\n💻Usage: *.hack*")
+
+}));
